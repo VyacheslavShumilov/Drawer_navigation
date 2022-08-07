be92da67c12cfb70e42e1d7b497ffc277a1ea2f3
user.email <vvshumilov@mail.ru>
2022-08-07 13:25:38 +0300

    07.08.2022 Add Fragments

--- app/src/main/java/com/hfad/drawernavigation/MainActivity.kt
+++ app/src/main/java/com/hfad/drawernavigation/MainActivity.kt
@@ -7,8 +7,11 @@ import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.core.view.GravityCompat
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 import com.google.android.material.navigation.NavigationView
 import com.hfad.drawernavigation.databinding.ActivityMainBinding
+import com.hfad.drawernavigation.fragment.*
 
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
@@ -61,14 +64,36 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
             R.id.nav_profile ->{
+                loadFragment(ProfileFragment(), supportFragmentManager)
                 Toast.makeText(this, "Profile", Toast.LENGTH_SHORT).show()
             }
-
             R.id.nav_profile_edit -> {
+                loadFragment(ProfileEditFragment(), supportFragmentManager)
                 Toast.makeText(this, "Profile Edit", Toast.LENGTH_SHORT).show()
+            }
+            R.id.nav_Favourite -> {
+                loadFragment(FavouriteFragment(), supportFragmentManager)
+            }
+            R.id.nav_setting -> {
+                loadFragment(SettingFragment(), supportFragmentManager)
+            }
+            R.id.nav_phone -> {
+                loadFragment(PhoneFragment(), supportFragmentManager)
+            }
+            R.id.nav_email -> {
+                loadFragment(EmailFragment(), supportFragmentManager)
+            }
+            R.id.nav_site -> {
+                loadFragment(SiteFragment(), supportFragmentManager)
             }
         }
         binding.drawerLayout.closeDrawer(GravityCompat.START)
         return true
+    }
+
+    private fun loadFragment(fragment: Fragment, fragmentManager: FragmentManager) {
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.nvView, fragment)
+        fragmentTransaction.commit()
     }
 }
